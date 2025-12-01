f1ededb0c71a88e4ac61980b762c134ea12d4aae
bwright-scottlogic <bwright@scottlogic.com>
2025-12-01 11:39:48 +0000

    Build solution and Complete day1.

--- day1/App.cs
+++ day1/App.cs
@@ -1,6 +1,30 @@
-﻿namespace day1;
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace day1;
 
 public class App
 {
-    
+    public static void PrintSln(string puzzleInput)
+    {
+        var startingPosition = 50;
+        var zeros = 0;
+        Regex inputMatcher = new(@"([LR])(\d+)");
+        var inputs = puzzleInput.Split(["\r\n", "\r", "\n", " "], StringSplitOptions.None);
+
+        foreach (var input in inputs)
+        {
+            var matchedInput = inputMatcher.Match(input);
+
+            var endingPosition = matchedInput.Groups[1].Value == "L"
+                ? startingPosition - IntegerType.FromString(matchedInput.Groups[2].Value)
+                : startingPosition + IntegerType.FromString(matchedInput.Groups[2].Value);
+
+            if (endingPosition % 100 == 0) zeros++;
+
+            startingPosition = endingPosition;
+        }
+
+        Console.WriteLine(zeros);
+    }
 }
---
+++ day1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using day1;
+
+Console.WriteLine("Input puzzle!");
+
+var input = Console.ReadLine();
+
+App.PrintSln(input);
